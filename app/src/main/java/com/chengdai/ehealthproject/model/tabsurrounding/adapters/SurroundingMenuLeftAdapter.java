25f02713e079ca4d92415461ffa103b577bdfb81
18984955240 <18984955240@qq.com>
2017-06-10 21:02:45 +0800

    周边banner接口添加，商户分类接口，商户详情页面

--- app/src/main/java/com/chengdai/ehealthproject/model/tabsurrounding/adapters/SurroundingMenuLeftAdapter.java
+++ app/src/main/java/com/chengdai/ehealthproject/model/tabsurrounding/adapters/SurroundingMenuLeftAdapter.java
@@ -16,13 +16,7 @@ import java.util.List;
 
 public class SurroundingMenuLeftAdapter extends CommonAdapter {
 
-
     private String typeName;
-
-    public String getTypeName() {
-        return typeName;
-    }
-
 
     public void setTypeName(String typeName) {
         this.typeName = typeName;
@@ -36,7 +30,6 @@ public class SurroundingMenuLeftAdapter extends CommonAdapter {
 
     @Override
     protected void convert(ViewHolder viewHolder, Object item, int position) {
-
 
         TextView tv=viewHolder.getView(R.id.tv_txt);
         StoreTypeModel model= (StoreTypeModel) item;
