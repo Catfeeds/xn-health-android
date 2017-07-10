f1b96f01ba4223b70b47d011c7bc2a4d6c1885c7
18984955240 <18984955240@qq.com>
2017-07-10 17:03:29 +0800

    血压测试添加

--- app/src/main/java/com/chengdai/ehealthproject/model/healthmanager/acitivitys/XueyaCalculateActivity.java
+++ app/src/main/java/com/chengdai/ehealthproject/model/healthmanager/acitivitys/XueyaCalculateActivity.java
@@ -32,7 +32,6 @@ public class XueyaCalculateActivity extends AbsBaseActivity{
         context.startActivity(intent);
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
